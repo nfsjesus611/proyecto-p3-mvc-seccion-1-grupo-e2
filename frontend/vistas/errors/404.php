4102266335dbb1602e45f90b82eef0cd02b700f5
Jesus Rodriguez <42216770+nfsjesus611@users.noreply.github.com>
2026-04-30 23:27:35 -0400

    feat(views): diseñar página de manejo de error 404
    Contexto: Para 404.php en la carpeta errors

---
+++ frontend/vistas/errors/404.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<base href="<?php echo url(); ?>">
+	<link rel="stylesheet" href="<?php echo url('frontend/asset/css/bootstrap.min.css'); ?>">
+	<link rel="stylesheet" href="<?php echo url('frontend/asset/css/style-color.css'); ?>">
+	<link rel="stylesheet" href="<?php echo url('frontend/asset/css/style.css'); ?>">
+	<link rel="stylesheet" href="<?php echo url('frontend/asset/css/fontawesome.all.min.css'); ?>">
+</head>
+<body>
+	<section class="intro d-flex justify-content-center align-items-center">
+	  <div class="bg-image h-100">
+	    <div class="mask d-flex align-items-center h-100" style="background-color: #f3f2f2;">
+	      <div class="container-fluid">
+	        <div class="row d-flex justify-content-center align-items-center">
+	          <div class="col-12 col-lg-9 col-xl-8">
+	            <div class="card" style="border-radius: 1rem;">
+	              <div class="row g-0">
+	                <div class="col-md-4">
+						<img src="<?php echo url('frontend/asset/img/estacion-pasajeros-usuarios-Metro-Los-Teques.jpg') ?>" alt="login form" class="img-fluid" style="border-top-left-radius: 1rem; border-bottom-left-radius: 1rem;width: 100%;height: 100%;object-fit: cover;background-blend-mode: multiply;" />
+	                </div>
+	                <div class="col-md-8 d-flex align-items-center">
+	                  <div class="card-body py-5 px-4 p-md-5">
+	                  	<div class="row">
+	                  		<div class="col">
+	                  			<div class="row justify-content-center align-items-center">
+	                  				<div class="col w-100">
+			                  			<img class="mx-auto d-block" src="<?php echo url('frontend/asset/img/DF-LiCB6_400x400.jpg') ?>" alt="login form2" style="width: 20%; height: 20%;">
+	                  				</div>
+	                  			</div>
+	                  			<div class="row">
+	                  				<h1>Error 404</h1>
+	                  				<p>Pagina no encontrada</p>
+	                  			</div>
+
+									
+								<p class="">
+									<a href="<?php echo url('backend/'); ?>"> Ir a la página principal</a>
+								</p>
+	                  		</div>
+	                  	</div>
+	                  </div>
+	                </div>
+	              </div>
+	            </div>
+	          </div>
+	        </div>
+	      </div>
+	    </div>
+	  </div>
+	</section>
+</body>
+</html>
