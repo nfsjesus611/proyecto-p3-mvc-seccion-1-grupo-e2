04956e36ef073c839b2259256344f01aa6a9606b
Jesus Rodriguez <42216770+nfsjesus611@users.noreply.github.com>
2026-04-30 23:26:03 -0400

    feat(views): crear componente parcial del menú de navegación
    Contexto: Para el archivo menu.php en vistas/auth/partials/

---
+++ frontend/vistas/auth/partials/menu.php
@@ -0,0 +1,12 @@
+<div class="col-3 p-0 h-100 border border-0 border-end bg-red-lighten-1">
+	<div class="accordion" id="accordionExample">
+		<div class="box row align-content-center">
+			<!-- <h1 class="text-center">MLT</h1> -->
+			<img class="mx-auto d-block" src="<?php echo url('frontend/asset/img/DF-LiCB6_400x400.jpg') ?>" alt="login form2" style="width: 100%; height: 100%;">
+		</div>
+		<ul class="nav nav-tabs flex-column border-0 rounded-0">
+			<li class="nav-item bg-red-lighten-1 border-0 border-bottom rounded-0 p-1"><a class="nav-link text-white border-0 rounded-0" href="<?php echo url('backend/admin'); ?>">Panel</a></li>
+			<li class="nav-item bg-red-lighten-1 border-0 border-bottom rounded-0 p-1"><a class="nav-link text-white border-0 rounded-0" href="<?php echo url('backend/auth/logout'); ?>">Salir</a></li>
+		</ul>
+	</div>
+</div>
